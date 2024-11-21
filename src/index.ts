56f212b1b0d6dd9e7df0cda1b66b9e45475c141f
Aurélien Vasseur <aurelienvasseur60@gmail.com>
2024-11-21 15:11:08 +0100

    Main function updated to use new services

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,47 @@
-import { saveJson } from "./utils/jsonUtils";
-import WaltioService from "./services/waltioService";
-import parseTransactions from "./parseTransactions";
-import { calculateValuations } from "./valuation";
+import * as p from "path";
+import { getRowsFromExcelFile } from "./utils/excelUtils";
+import { config } from "./config";
+import WaltioService from "./services/waltio.service";
+import MetricsService from "./services/metrics.service";
+import TransactionsService from "./services/transactions.service";
+import { cleanOutput, generateUniqueTimestamp, save } from "./utils/jsonUtils";
 
 const main = async () => {
-  const transactions = await WaltioService.getTransactions();
+  const timestamp = generateUniqueTimestamp();
+  await cleanOutput(timestamp);
 
-  const result = parseTransactions(transactions);
-  const valuation = calculateValuations(result);
+  // Parse transactions
+  const path: string = p.resolve(__dirname, `../${config.filePath}`);
+  const rows = await getRowsFromExcelFile(path);
+  const transactions = WaltioService.getTransactions(rows);
+  
+  // Compute metrics
+  const volumes = MetricsService.computeVolumes(transactions);
+  const tokens = TransactionsService.getTokens(transactions);
 
-  saveJson(result, "t-all");
-  saveJson(result.overview, "t-overview");
-  saveJson(result.tokens, "t-tokens");
-  saveJson(result.groups, "t-groups");
-  saveJson(valuation, "t-valuation")
+  save(timestamp, transactions, "transactions");
+  save(timestamp, volumes, "volumes");
+  save(timestamp, tokens, "tokens");
+
+  const tokensMerged = [
+    ...new Set([
+      ...tokens.tokensReceived,
+      ...tokens.tokensSent,
+      ...tokens.tokensFees,
+    ]),
+  ];
+  tokensMerged.map((token) => {
+    const tokenTransactions = TransactionsService.getTokenTransactions(
+      transactions,
+      token
+    );
+    const tokenMetrics = MetricsService.computeTokenMetrics(
+      transactions,
+      token
+    );
+    save(timestamp, tokenTransactions, token, "transactions");
+    save(timestamp, tokenMetrics, token, "metrics");
+  });
 };
 
 main().then();
--- src/utils/jsonUtils.ts
+++ src/utils/jsonUtils.ts
@@ -1,23 +1,113 @@
+import { Dirent, Stats } from "fs";
+import * as p from "path";
+
 var fs = require("fs");
 
 /**
- * Save an object into a new JSON file into the output folder
- * @param obj JSON object to save
- * @param fileName
- * @returns
+ * Generates a unique timestamp string in the format "YYYY-MM-DD_HH-mm-ss".
+ * @returns {string} A unique timestamp string.
  */
-export async function saveJson(
-  obj: any,
-  fileName: string
-): Promise<void> {
-  return new Promise((resolve, reject) => {
-    const json = JSON.stringify(obj);
-    fs.writeFile(`./output/${fileName}.json`, json, (err: any) => {
-      if (err) {
-        reject(err);
-      } else {
-        resolve();
+export function generateUniqueTimestamp() {
+  // Get the current date and time
+  const currentDate = new Date();
+  // Extract the year, month, day, hour, minute, and second
+  const year = currentDate.getFullYear();
+  const month = String(currentDate.getMonth() + 1).padStart(2, "0");
+  const day = String(currentDate.getDate()).padStart(2, "0");
+  const hour = String(currentDate.getHours()).padStart(2, "0");
+  const minute = String(currentDate.getMinutes()).padStart(2, "0");
+  const second = String(currentDate.getSeconds()).padStart(2, "0");
+  // Construct the unique timestamp string
+  return `${year}-${month}-${day}_${hour}-${minute}-${second}`;
+}
+
+/**
+ * Clean directory (create or remove content)
+ * @param path Path
+ * @param removeContent Remove the content of the directories if true and if the directory exists
+ */
+async function prepareDirectory(path: string, removeContent: boolean) {
+  try {
+    //Check if directory exists
+    const exists: boolean = (() => {
+      try {
+        const stats: Stats = fs.statSync(path);
+        return stats.isDirectory();
+      } catch (error) {
+        return false;
       }
-    });
-  });
+    })();
+
+    if (exists) {
+      if (!removeContent) return;
+      // Directory exists, remove its contents
+      const files: Dirent[] = fs.readdirSync(path, {
+        withFileTypes: true,
+      });
+      for (const file of files) {
+        const filePath = p.join(path, file.name);
+        fs.rmSync(filePath, { recursive: true });
+      }
+    } else {
+      // Directory does not exist, create it
+      try {
+        // to avoid conflict if a file has the expected name
+        fs.rmSync(path, { recursive: true });
+      } catch (error) {}
+      fs.mkdirSync(path);
+    }
+  } catch (error) {
+    throw error;
+  }
+}
+
+/**
+ * Clean the output directory
+ * @param timestamp Timestamp string
+ */
+export async function cleanOutput(timestamp: string) {
+  try {
+    const outputPath = p.join(__dirname, "../../output-test");
+    const timestampPath = p.join(outputPath, timestamp);
+    const transactionsPath = p.join(timestampPath, "transactions");
+    const metricsPath = p.join(timestampPath, "metrics");
+    await prepareDirectory(outputPath, false);
+    await prepareDirectory(timestampPath, true);
+    await prepareDirectory(transactionsPath, true);
+    await prepareDirectory(metricsPath, true);
+  } catch (error) {
+    console.error("Error preparing the output directory:", error);
+    throw error;
+  }
+}
+
+/**
+ * Save an object into a new JSON file in the output folder
+ * @param timestamp Timestamp string
+ * @param obj JSON object to save
+ * @param fileName File name
+ */
+export async function save(
+  timestamp: string,
+  obj: any,
+  fileName: string,
+  sub?: "transactions" | "metrics"
+) {
+  try {
+    // Replace disallowed characters and spaces with an underscore
+    const securedFileName = fileName
+      .replaceAll(" ", "_")
+      .replaceAll(/[\\\/:\*\?"<>\|]/g, "_");
+
+    const subpath = sub ? `${sub}/${securedFileName}` : securedFileName;
+    const path = p.join(
+      __dirname,
+      `../../output-test/${timestamp}/${subpath}.json`
+    );
+
+    const json = JSON.stringify(obj, null, 2);
+    fs.writeFileSync(path, json);
+  } catch (error) {
+    console.error("Error saving " + fileName);
+  }
 }
