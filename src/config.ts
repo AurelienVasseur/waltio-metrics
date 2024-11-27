630f12f3618f746f5f880db35b9c100f42da877f
Aurélien Vasseur <aurelienvasseur60@gmail.com>
2024-11-27 16:21:30 +0100

    Useless configuration removed

--- src/config.ts
+++ src/config.ts
@@ -18,26 +18,6 @@ interface Config {
    * Define token aliases.
    */
   tokenAliases: Record<string, string[]>;
-  /**
-   * A record of expected quantities for specific tokens.
-   * The key is the token symbol and the value is the expected quantity.
-   */
-  expectedQuantities: Record<string, number>;
-  /**
-   * Define the groups and their composition.
-   */
-  groups: Record<string, string[]>;
-  /**
-   * Represents a single pricing scenario with a description
-   * and expected prices for tokens.
-   */
-  scenarios: Record<
-    string,
-    {
-      description: string;
-      prices: Record<string, number>;
-    }
-  >;
 }
 
 const is_test = process.env.NODE_ENV === "test";
